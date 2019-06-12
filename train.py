6a71cda6fc9c7d2fceb19a8c233b944a5d2618b8
chenxinyang123 <943734637@qq.com>
2019-06-12 16:43:12 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -18,9 +18,12 @@ parser.add_argument('--src', type=str, default='A', metavar='S',
                     help='source dataset')
 parser.add_argument('--tgt', type=str, default='C', metavar='S',
                     help='target dataset')
+parser.add_argument('--num_iter', type=int, default=50002,
+                    help='max iter_num')
 args = parser.parse_args()
 
 def get_datasetname(args):
+    noe = 0
     visda = False
     office = False
     A = './data/Art.txt'
@@ -54,9 +57,11 @@ def get_datasetname(args):
         office = True
     elif args.src == 'd':
         src = d
+        noe = noe + 1
         office = True
     if args.tgt == 'a':
         tgt = a
+        noe = noe + 1
     elif args.tgt == 'w':
         tgt = w
     elif args.tgt == 'd':
@@ -67,10 +72,10 @@ def get_datasetname(args):
         src = visda_train
         tgt = visda_test
         visda = True
-    return src,tgt,office,visda
+    return src,tgt,office,visda,noe
 
 
-src,tgt,office,visda = get_datasetname(args)
+src,tgt,office,visda,noe = get_datasetname(args)
 
 batch_size = {"train": 36, "val": 36, "test": 4}
 for i in range(10):
@@ -122,6 +127,7 @@ if office == True:
     dset_classes = range(31)
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+max_iter = args.num_iter
 
 def Visda_test(loader, model,iter_num):
     with torch.no_grad():
@@ -281,10 +287,6 @@ def calc_coeff(iter_num, high=1.0, low=0.0, alpha=10.0, max_iter=10000.0):
     return np.float(2.0 * (high - low) / (1.0 + np.exp(-alpha * iter_num / max_iter)) - (high - low) + low)
 
 
-def srpolicy(iter_num, max_iter, init_lr=0.005):
-    """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
-    lr = (iter_num / max_iter) * 4 * init_lr + init_lr
-    return lr
 
 def CDAN(input_list, ad_net, entropy=None, coeff=None, random_layer=None):
     softmax_output = input_list[1].detach()
@@ -326,83 +328,187 @@ def grl_hook(coeff):
 
     return fun1
 
-
-num_features = 2048
-DANN = BSP_CDAN(num_features)
-DANN = DANN.to(device)
-ad_net = AdversarialNetwork(256 * len(dset_classes), 1024)
-ad_net = ad_net.to(device)
-DANN.train(True)
-ad_net.train(True)
-criterion = {"classifier": nn.CrossEntropyLoss(), "adversarial": nn.BCELoss()}
-optimizer_dict = [{"params": filter(lambda p: p.requires_grad, DANN.model_fc.parameters()), "lr": 0.1},
-                  {"params": filter(lambda p: p.requires_grad, DANN.bottleneck_layer.parameters()), "lr": 1},
-                  {"params": filter(lambda p: p.requires_grad, DANN.classifier_layer.parameters()), "lr": 1},
-                  {"params": filter(lambda p: p.requires_grad, ad_net.parameters()), "lr": 1}]
-optimizer = optim.SGD(optimizer_dict, lr=0.1, momentum=0.9, weight_decay=0.0005, nesterov=True)
-train_cross_loss = train_transfer_loss = train_total_loss = train_sigma = 0.0
-len_source = len(dset_loaders["train"]) - 1
-len_target = len(dset_loaders["val"]) - 1
-param_lr = []
-iter_source = iter(dset_loaders["train"])
-iter_target = iter(dset_loaders["val"])
-for param_group in optimizer.param_groups:
-    param_lr.append(param_group["lr"])
-test_interval = 500
-num_iter = 50002
-for iter_num in range(1, num_iter + 1):
-    DANN.train(True)
-    if office == False:
-        optimizer = inv_lr_scheduler(param_lr, optimizer, iter_num, init_lr=0.003, gamma=0.0001, power=0.75,
-                                 weight_decay=0.0005)
-    else:
-        optimizer = inv_lr_scheduler(param_lr, optimizer, iter_num, init_lr=0.003, gamma=0.001, power=0.75,
+if noe < 2:
+    num_features = 2048
+    net = BSP_CDAN(num_features)
+    net = net.to(device)
+    ad_net = AdversarialNetwork(256 * len(dset_classes), 1024)
+    ad_net = ad_net.to(device)
+    net.train(True)
+    ad_net.train(True)
+    criterion = {"classifier": nn.CrossEntropyLoss(), "adversarial": nn.BCELoss()}
+    optimizer_dict = [{"params": filter(lambda p: p.requires_grad, net.model_fc.parameters()), "lr": 0.1},
+                      {"params": filter(lambda p: p.requires_grad, net.bottleneck_layer.parameters()), "lr": 1},
+                      {"params": filter(lambda p: p.requires_grad, net.classifier_layer.parameters()), "lr": 1},
+                      {"params": filter(lambda p: p.requires_grad, ad_net.parameters()), "lr": 1}]
+    optimizer = optim.SGD(optimizer_dict, lr=0.1, momentum=0.9, weight_decay=0.0005, nesterov=True)
+    train_cross_loss = train_transfer_loss = train_total_loss = train_sigma = 0.0
+    len_source = len(dset_loaders["train"]) - 1
+    len_target = len(dset_loaders["val"]) - 1
+    param_lr = []
+    iter_source = iter(dset_loaders["train"])
+    iter_target = iter(dset_loaders["val"])
+    for param_group in optimizer.param_groups:
+        param_lr.append(param_group["lr"])
+    test_interval = 500
+    num_iter = max_iter
+    for iter_num in range(1, num_iter + 1):
+        net.train(True)
+        if office == False:
+            optimizer = inv_lr_scheduler(param_lr, optimizer, iter_num, init_lr=0.003, gamma=0.0001, power=0.75,
                                      weight_decay=0.0005)
-    optimizer.zero_grad()
-    if iter_num % len_source == 0:
-        iter_source = iter(dset_loaders["train"])
-    if iter_num % len_target == 0:
-        iter_target = iter(dset_loaders["val"])
-    data_source = iter_source.next()
-    data_target = iter_target.next()
-    inputs_source, labels_source = data_source
-    inputs_target, labels_target = data_target
-    inputs = torch.cat((inputs_source, inputs_target), dim=0)
-    dc_target = torch.from_numpy(np.array([[1], ] * batch_size["train"] + [[0], ] * batch_size["train"])).float()
-    inputs = inputs.to(device)
-    labels = labels_source.to(device)
-    dc_target = dc_target.to(device)
-    feature, outC = DANN(inputs)
-    feature_s = feature.narrow(0, 0, int(feature.size(0) / 2))
-    feature_t = feature.narrow(0, int(feature.size(0) / 2), int(feature.size(0) / 2))
-    _, s_s, _ = torch.svd(feature_s)
-    _, s_t, _ = torch.svd(feature_t)
-    sigma = torch.pow(s_s[0], 2) + torch.pow(s_t[0], 2)
-    sigma_loss = 0.0001 * sigma
-    classifier_loss = criterion["classifier"](outC.narrow(0, 0, batch_size["train"]), labels)
-    total_loss = classifier_loss
-    softmax_out = nn.Softmax(dim=1)(outC)
-    entropy = Entropy(softmax_out)
-    coeff = calc_coeff(iter_num)
-    transfer_loss = CDAN([feature, softmax_out], ad_net, entropy, coeff, random_layer=None)
-    total_loss = total_loss + transfer_loss + sigma_loss
-    total_loss.backward()
-    optimizer.step()
-    train_cross_loss += classifier_loss.item()
-    train_transfer_loss += transfer_loss.item()
-    train_total_loss += total_loss.item()
-    train_sigma += sigma_loss.item()
-    if iter_num % test_interval == 0:
-        print(
-        "Iter {:05d}, Average Cross Entropy Loss: {:.4f}; Average Transfer Loss: {:.4f}; Average Sigma Loss: {:.4f}; Average Training Loss: {:.4f}".format(
-            iter_num, train_cross_loss / float(test_interval), train_transfer_loss / float(test_interval),
-                      train_sigma / float(test_interval),
-                      train_total_loss / float(test_interval)))
-        train_cross_loss = train_transfer_loss = train_total_loss = train_sigma = 0.0
-    if (iter_num % 1000) == 0:
-        DANN.eval()
-        if visda == True:
-            Visda_test(dset_loaders, DANN.predict_layer,iter_num)
         else:
-            test_acc = test_target(dset_loaders, DANN.predict_layer)
-            print('test_acc:%.4f'%(test_acc))
+            optimizer = inv_lr_scheduler(param_lr, optimizer, iter_num, init_lr=0.003, gamma=0.001, power=0.75,
+                                         weight_decay=0.0005)
+        optimizer.zero_grad()
+        if iter_num % len_source == 0:
+            iter_source = iter(dset_loaders["train"])
+        if iter_num % len_target == 0:
+            iter_target = iter(dset_loaders["val"])
+        data_source = iter_source.next()
+        data_target = iter_target.next()
+        inputs_source, labels_source = data_source
+        inputs_target, labels_target = data_target
+        inputs = torch.cat((inputs_source, inputs_target), dim=0)
+        dc_target = torch.from_numpy(np.array([[1], ] * batch_size["train"] + [[0], ] * batch_size["train"])).float()
+        inputs = inputs.to(device)
+        labels = labels_source.to(device)
+        dc_target = dc_target.to(device)
+        feature, outC = net(inputs)
+        feature_s = feature.narrow(0, 0, int(feature.size(0) / 2))
+        feature_t = feature.narrow(0, int(feature.size(0) / 2), int(feature.size(0) / 2))
+        _, s_s, _ = torch.svd(feature_s)
+        _, s_t, _ = torch.svd(feature_t)
+        sigma = torch.pow(s_s[0], 2) + torch.pow(s_t[0], 2)
+        sigma_loss = 0.0001 * sigma
+        classifier_loss = criterion["classifier"](outC.narrow(0, 0, batch_size["train"]), labels)
+        total_loss = classifier_loss
+        softmax_out = nn.Softmax(dim=1)(outC)
+        entropy = Entropy(softmax_out)
+        coeff = calc_coeff(iter_num)
+        transfer_loss = CDAN([feature, softmax_out], ad_net, entropy, coeff, random_layer=None)
+        total_loss = total_loss + transfer_loss + sigma_loss
+        total_loss.backward()
+        optimizer.step()
+        train_cross_loss += classifier_loss.item()
+        train_transfer_loss += transfer_loss.item()
+        train_total_loss += total_loss.item()
+        train_sigma += sigma_loss.item()
+        if iter_num % test_interval == 0:
+            print(
+            "Iter {:05d}, Average Cross Entropy Loss: {:.4f}; Average Transfer Loss: {:.4f}; Average Sigma Loss: {:.4f}; Average Training Loss: {:.4f}".format(
+                iter_num, train_cross_loss / float(test_interval), train_transfer_loss / float(test_interval),
+                          train_sigma / float(test_interval),
+                          train_total_loss / float(test_interval)))
+            train_cross_loss = train_transfer_loss = train_total_loss = train_sigma = 0.0
+        if (iter_num % 500) == 0:
+            net.eval()
+            if visda == True:
+                Visda_test(dset_loaders, net.predict_layer,iter_num)
+            else:
+                test_acc = test_target(dset_loaders, net.predict_layer)
+                print('test_acc:%.4f'%(test_acc))
+else:
+    class GRL(nn.Module):
+        def __init__(self, num_features):
+            super(GRL, self).__init__()
+            self.model_fc = model.Resnet50Fc()
+            # for param in self.model_fc.parameters():
+            #    param.requires_grad=False
+            self.bottleneck_layer1 = nn.Linear(num_features, 256)
+            self.bottleneck_layer1.weight.data.normal_(0, 0.005)
+            self.bottleneck_layer1.bias.data.fill_(0.1)
+            self.bottleneck_layer = nn.Sequential(self.bottleneck_layer1, nn.ReLU(), nn.Dropout(0.5))
+            self.classifier_layer = nn.Linear(256, len(dset_classes))
+            self.classifier_layer.weight.data.normal_(0, 0.01)
+            self.classifier_layer.bias.data.fill_(0.0)
+            self.predict_layer = nn.Sequential(self.model_fc, self.bottleneck_layer, self.classifier_layer)
+            self.ad_layer1 = nn.Linear(256 * len(dset_classes), 1024)
+            self.ad_layer2 = nn.Linear(1024, 1024)
+            self.ad_layer3 = nn.Linear(1024, 1)
+            self.ad_layer1.weight.data.normal_(0, 0.01)
+            self.ad_layer2.weight.data.normal_(0, 0.01)
+            self.ad_layer3.weight.data.normal_(0, 0.3)
+            self.ad_layer1.bias.data.fill_(0.0)
+            self.ad_layer2.bias.data.fill_(0.0)
+            self.ad_layer3.bias.data.fill_(0.0)
+            self.ad_net = nn.Sequential(self.ad_layer1, nn.ReLU(), nn.Dropout(0.5), self.ad_layer2, nn.ReLU(),
+                                        nn.Dropout(0.5), self.ad_layer3,
+                                        nn.Sigmoid())
+            self.grl = ad.AdversarialLayer(high=1.0)
+
+        def forward(self, x):
+            feature = self.model_fc(x)
+            out = self.bottleneck_layer(feature)
+            outC = self.classifier_layer(out)
+            softmax_output = nn.Softmax(dim=1)(outC).detach()
+            op_out = torch.bmm(softmax_output.unsqueeze(2), out.unsqueeze(1))
+            outD = self.ad_net(self.grl(op_out.view(-1, softmax_output.size(1) * out.size(1))))
+            return (outC, outD, out)
+
+    num_features = 2048
+    net = GRL(num_features)
+    net = net.to(device)
+
+    net.train(True)
+    criterion = {"classifier": nn.CrossEntropyLoss(), "adversarial": nn.BCELoss()}
+    optimizer_dict = [{"params": filter(lambda p: p.requires_grad, net.model_fc.parameters()), "lr": 0.1},
+                      {"params": filter(lambda p: p.requires_grad, net.bottleneck_layer.parameters()), "lr": 1},
+                      {"params": filter(lambda p: p.requires_grad, net.classifier_layer.parameters()), "lr": 1},
+                      {"params": filter(lambda p: p.requires_grad, net.ad_net.parameters()), "lr": 1}]
+    optimizer = optim.SGD(optimizer_dict, lr=0.1, momentum=0.9, weight_decay=0.0005)
+    train_cross_loss = train_transfer_loss = train_total_loss = train_sigma = 0.0
+    len_source = len(dset_loaders["train"]) - 1
+    len_target = len(dset_loaders["val"]) - 1
+    param_lr = []
+    iter_source = iter(dset_loaders["train"])
+    iter_target = iter(dset_loaders["val"])
+    for param_group in optimizer.param_groups:
+        param_lr.append(param_group["lr"])
+    test_interval = 500
+    num_iter = max_iter
+    for iter_num in range(1, num_iter + 1):
+        net.train(True)
+        optimizer = inv_lr_scheduler(param_lr, optimizer, iter_num, init_lr=0.003, gamma=0.001, power=0.75)
+        optimizer.zero_grad()
+        if iter_num % len_source == 0:
+            iter_source = iter(dset_loaders["train"])
+        if iter_num % len_target == 0:
+            iter_target = iter(dset_loaders["val"])
+        data_source = iter_source.next()
+        data_target = iter_target.next()
+        inputs_source, labels_source = data_source
+        inputs_target, labels_target = data_target
+        inputs = torch.cat((inputs_source, inputs_target), dim=0)
+        dc_target = torch.from_numpy(np.array([[1], ] * batch_size["train"] + [[0], ] * batch_size["train"])).float()
+        inputs = inputs.to(device)
+        labels = labels_source.to(device)
+        dc_target = dc_target.to(device)
+        outC, outD, feature = net(inputs)
+        feature_s = feature.narrow(0, 0, int(feature.size(0) / 2))
+        feature_t = feature.narrow(0, int(feature.size(0) / 2), int(feature.size(0) / 2))
+        _, s_s, _ = torch.svd(feature_s)
+        _, s_t, _ = torch.svd(feature_t)
+        sigma = torch.pow(s_s[0], 2) + torch.pow(s_t[0], 2)
+        sigma_loss = 0.0001 * sigma
+        classifier_loss = criterion["classifier"](outC.narrow(0, 0, batch_size["train"]), labels)
+        total_loss = classifier_loss
+        transfer_loss = nn.BCELoss()(outD, dc_target)
+        total_loss = total_loss + transfer_loss + sigma_loss
+        total_loss.backward()
+        optimizer.step()
+        train_cross_loss += classifier_loss.item()
+        train_transfer_loss += transfer_loss.item()
+        train_total_loss += total_loss.item()
+        train_sigma += sigma_loss.item()
+        if iter_num % test_interval == 0:
+            print(
+            "Iter {:05d}, Average Cross Entropy Loss: {:.4f}; Average Transfer Loss: {:.4f}; Average Sigma Loss: {:.4f}; Average Training Loss: {:.4f}".format(
+                iter_num, train_cross_loss / float(test_interval), train_transfer_loss / float(test_interval),
+                          train_sigma / float(test_interval),
+                          train_total_loss / float(test_interval)))
+            train_cross_loss = train_transfer_loss = train_total_loss = train_sigma = 0.0
+        if (iter_num % 500) == 0:
+            net.eval()
+            test_acc = test_target(dset_loaders, net.predict_layer)
+            print('test_acc:%.4f' % (test_acc))
